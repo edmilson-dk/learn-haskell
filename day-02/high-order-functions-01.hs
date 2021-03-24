1c09e9d48ae352fc9a948d8d1796a56ca5ab7d8f
edmilson-dk <droidvidaboa@gmail.com>
2021-03-24 15:17:47 -0300

    feat: renamed functions

--- day-02/high-order-functions-01.hs
+++ day-02/high-order-functions-01.hs
@@ -1,10 +1,10 @@
-doubleNumber :: (t1 -> t2 -> t3) -> t1 -> t2 -> t3
-doubleNumber fn = fn
+applyTwoNumbers :: Num t => (t -> t) -> t -> t -> t
+applyTwoNumbers fn x y = fn(fn x + y)
 
-printNumber :: Num a => a -> a -> a
-printNumber x y = x + y
+returnResult :: p -> p
+returnResult x = x
 
 main :: IO ()
 main = do
-  let msg = doubleNumber printNumber 10 30
+  let msg = applyTwoNumbers returnResult 10 30
   print msg
