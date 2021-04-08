792bb6bd17e4aff24f2be7ac1ba19e56a5d89774
edmilson-dk <droidvidaboa@gmail.com>
2021-04-08 13:18:05 -0300

    feat: learned zip method of tuple

--- day-06/zip-tuple.hs
+++ day-06/zip-tuple.hs
@@ -1,8 +1,16 @@
 myTuple :: [(Integer , Integer)]
 myTuple = zip [1 .. 5] [31 .. 35]
 
+{-
+  which right triangle that has integers for all sides and all sides equal to or smaller than 10 has a perimeter of 24? First,
+ let's try generating all triangles with sides equal to or smaller than 10:
+-}
+
 triangles :: [(Integer, Integer, Integer)]
-triangles = [(a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10], a ^2 + b ^2 == c ^ 2, a + b + c == 24]
+triangles = [
+  (a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10],
+  a ^2 + b ^2 == c ^ 2,
+  a + b + c == 24]
 
 main :: IO ()
 main = do 
